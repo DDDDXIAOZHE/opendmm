72654a9019b1a481f600f7e587dd22bcd684df14
Jun Zhou <pinepara@gmail.com>
2014-05-18 20:15:01 -0700

    Ideapocket: IDBD

--- lib/opendmm/makers/ideapocket.rb
+++ lib/opendmm/makers/ideapocket.rb
@@ -29,10 +29,10 @@ module OpenDMM
             },
             label:        specs["レーベル"],
             maker:        "Ideapocket",
-            movie_length: ChronicDuration.parse(specs["収録時間"]),
+            movie_length: ChronicDuration.parse(specs["収録時間"]["DVD"]),
             page:         page_uri.to_s,
-            product_id:   specs["品番"],
-            release_date: Date.parse(specs["発売日"]),
+            product_id:   specs["品番"]["DVD"],
+            release_date: Date.parse(specs["発売日"]["DVD"]),
             series:       specs["シリーズ"],
             title:        html.css("div#content-box h2.list-ttl").text.squish,
           }
@@ -42,9 +42,21 @@ module OpenDMM
 
         def self.parse_specs(html)
           specs = {}
-          html.xpath('//*[@id="navi-right"]/div[1]/p').text.remove(/\s*DVD/).lines do |line|
+          last_key = nil
+          html.xpath('//*[@id="navi-right"]/div[1]/p').text.lines do |line|
             if line =~ /(.*)：(.*)/
               specs[$1.squish] = $2.squish
+              last_key = $1.squish
+            else
+              specs[last_key] += line.squish
+            end
+          end
+          specs.each do |key, value|
+            tokens = value.squish.split(/(Blu-ray|DVD)/)
+            tokens = tokens.delete_if(&:empty?).map(&:squish)
+            case tokens.size
+            when 2,4
+              specs[key] = Hash[*tokens]
             end
           end
           specs
@@ -53,7 +65,7 @@ module OpenDMM
 
       def self.search(name)
         case name
-        when /IPZ-\d{3}/i
+        when /IPZ-\d{3}/i, /IDBD-\d{3}/i
           Parser.parse(Site.item(name))
         end
       end
