8952f6e9a51df9860c8a81f82f7016da6d59206d
Jun Zhou <pinepara@gmail.com>
2014-05-24 00:54:36 -0700

    Aurora: APAA

---
+++ test/fixtures/APAA.rb
@@ -0,0 +1,44 @@
+module Fixture
+  APAA = {
+    "APAA-182" => {
+      actresses:     {
+        "穂積ゆうり" => nil,
+      },
+      actress_types: [
+        "巨乳",
+        "ロリ",
+        "美乳",
+      ],
+      description:   "最近は育ちの良いお嬢様のAV出演が続きます！整えられた髪に、制服が似合いすぎの女の子、ゆうりちゃん[T：156・B：90（E）・W：57・H：87]です。しかしそのSEXは、まさに愛人のごとく妖艶で濃厚。清楚の象徴の制服と淫猥の対局性が、男の性欲を刺激して止みません。見事なEカップバストを揺らし、見せつけながらの騎乗位！奮い付きたくなるようなヒップを突きだしながら、奥まで突かれまくる激しいバック！自ら腰を浮かせ挿入感を高めようと夢中の正常位！そして顔に放たれた精を舐め、すすり、呑み込む姿は卑猥そのもの。寝起きのベッドでは、タップリと手コキとフェラを施して、硬くしたモノを奥まで迎え入れ、昨夜にも負けないエッチを披露です！！",
+      directors:     {
+        "葵刀樹" => nil,
+      },
+      genres:        [
+        "女子校生",
+        "ハメ撮り",
+      ],
+      images:        {
+        cover:   "http://rsc.aurora-pro.com/rsc/imgs/pkg/apaa-182/apaa-182_open_xl.jpg",
+        samples: [
+          "http://rsc.aurora-pro.com/rsc/imgs/scn/apaa-182/apaa-182_c1_f1_full_m.jpg",
+          "http://rsc.aurora-pro.com/rsc/imgs/scn/apaa-182/apaa-182_c2_f1_full_m.jpg",
+          "http://rsc.aurora-pro.com/rsc/imgs/scn/apaa-182/apaa-182_c3_f1_full_m.jpg",
+          "http://rsc.aurora-pro.com/rsc/imgs/scn/apaa-182/apaa-182_c4_f1_full_m.jpg",
+        ],
+      },
+      label:         nil,
+      maker:         "Apache Project",
+      movie_length:  109.minutes,
+      page:          "http://www.aurora-pro.com/shop/-/product/p/goods_id=APAA-182",
+      product_id:    "APAA-182",
+      release_date:  Date.new(2012, 11, 13),
+      scenes:        [
+        "顔射",
+        "フェラ",
+        "ランジェリー・肌着",
+        "イラマチオ",
+      ],
+      title:         "制服の愛人 ゆうり",
+    }
+  }
+end
