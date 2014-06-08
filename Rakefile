8f93a215b5978e2cb15ce7a856d5ab3fd1160039
Jun Zhou <pinepara@gmail.com>
2014-06-08 02:13:14 -0700

    AnnaAndHanako: ANND

--- Rakefile
+++ Rakefile
@@ -8,13 +8,23 @@ Rake::TestTask.new do |t|
 end
 
 namespace :test do
-  desc "Regenerate fixtures"
-  task :fixture do
-    Dir[File.dirname(__FILE__) + '/test/fixtures/*.json'].each do |path|
-      File.open(path, "w") do |file|
-        id = File.basename(path, ".json")
-        puts "Generating #{id}.json"
-        file.puts(JSON.pretty_generate(OpenDMM.search(id)))
+  namespace :fixture do
+    desc "Generate a fixture"
+    task :generate, [:id] do |t, args|
+      File.open(File.join(File.dirname(__FILE__) + "/test/fixtures/#{args[:id]}.json"), "w") do |file|
+        puts "Generating #{args[:id]}.json"
+        file.puts JSON.pretty_generate(OpenDMM.search(args[:id]))
+      end
+    end
+
+    desc "Regenerate all fixtures"
+    task :regenerate do
+      Dir[File.dirname(__FILE__) + '/test/fixtures/*.json'].each do |path|
+        File.open(path, "w") do |file|
+          id = File.basename(path, ".json")
+          puts "Generating #{id}.json"
+          file.puts JSON.pretty_generate(OpenDMM.search(id))
+        end
       end
     end
   end
---
+++ lib/opendmm/makers/anna_and_hanako.rb
@@ -0,0 +1,39 @@
+module OpenDMM
+  module Maker
+    module AnnaAndHanako
+      include Maker
+
+      module Site
+        include HTTParty
+        base_uri "anna-and-hanako.jp"
+
+        def self.item(name)
+          case name
+          when /(ANND)-?(\d{3})/i
+            get("/works/#{$1.downcase}/#{$1.downcase}#{$2}.html")
+          end
+        end
+      end
+
+      module Parser
+        def self.parse(content)
+          page_uri = content.request.last_uri
+          html = Nokogiri::HTML(Utils.force_utf8(content))
+          specs = Utils.hash_by_split(html.xpath('//*[@id="pake-bottom-box_r"]/dl/p').map(&:text))
+          return {
+            actresses:     specs["出演者"].split("/"),
+            code:          specs["品番"],
+            cover_image:   URI.join(page_uri, html.xpath('//*[@id="pake-bottom-box"]/dl/a').first["href"]).to_s,
+            description:   html.xpath('//*[@id="txt-bottom-box"]').text.squish,
+            directors:     specs["監督"].split("/"),
+            movie_length:  ChronicDuration.parse(specs["収録時間"]),
+            page:          page_uri.to_s,
+            release_date:  Date.parse(specs["発売日"]),
+            sample_images: html.xpath('//*[@id="mein-sanpuru-sam"]/a').map { |a| URI.join(page_uri, a["href"]).to_s },
+            title:         html.xpath('//*[@id="mein-left-new-release-box"]/div/div/h5').text.squish,
+          }
+        end
+      end
+    end
+  end
+end
