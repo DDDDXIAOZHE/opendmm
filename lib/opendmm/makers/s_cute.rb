1455b7f1a7fda885d5a744bec1bfe33f032dd88f
Jun Zhou <pinepara@gmail.com>
2014-06-27 01:14:37 -0700

    fix S-Cute

--- lib/opendmm/makers/s_cute.rb
+++ lib/opendmm/makers/s_cute.rb
@@ -19,10 +19,12 @@ module OpenDMM
         def self.parse(content)
           page_uri = content.request.last_uri
           html = Nokogiri::HTML(content)
+          code = page_uri.to_s.split('/').last
           specs = html.xpath('//*[@class="detail"]/article/p[2]').text.split('|')
           return {
-            code:            'S-Cute ' + page_uri.to_s.split('/').last,
-            cover_image:     html.at_css('#movie > div > div > div > a > img')['src'],
+            actresses:       parse_actresses(code),
+            code:            'S-Cute ' + code,
+            cover_image:     parse_cover_image(html, code),
             description:     html.css('//*[@class="detail"]/article/p[4]').text,
             maker:           'S-Cute',
             movie_length:    specs[1],
@@ -32,6 +34,26 @@ module OpenDMM
             thumbnail_image: html.at_css('div.cast > a > img')['src'],
             title:           html.css('div.detail > article > h3').text,
           }
+        end
+
+        def self.parse_actresses(code)
+          case code
+          when /^\d{3}[-_](\w+)[-_]\d{2}$/i, /^(?:ps\d|swm)[-_]\d{2}[-_](\w+)$/i
+            [ $1.match(/[a-z]+/)[0].humanize ]
+          else
+            nil
+          end
+        end
+
+        def self.parse_cover_image(html, code)
+          cover_img = html.at_css('#movie > div > div > div > a > img')
+          return cover_img['src'] if cover_img
+          case code
+          when /^(\d{3})[-_](\w+)[-_](\d{2})$/i
+            "http://static.s-cute.com/images/#{$1}_#{$2}/#{$1}_#{$2}_#{$3}/#{$1}_#{$2}_#{$3}_sample.jpg"
+          else
+            nil
+          end
         end
       end
     end
