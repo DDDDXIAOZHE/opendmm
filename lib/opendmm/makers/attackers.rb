fc0cbcd56bfc9fc950a342a17d0aec5f9558181b
Jun Zhou <pinepara@gmail.com>
2014-06-30 21:39:46 -0700

    fix a testcase

--- lib/opendmm/makers/attackers.rb
+++ lib/opendmm/makers/attackers.rb
@@ -12,8 +12,8 @@ def self.parse_product_html(html)
   {
     actresses:       specs['出演女優'].split,
     cover_image:     html.at_css('div#works_pake_box a#pake')['href'],
-    directors:       specs['監督'].split,
     description:     html.css('p.works_txt').text,
+    directors:       specs['監督'].split,
     genres:          specs['ジャンル'].split,
     label:           specs['レーベル'],
     maker:           'Attackers',
