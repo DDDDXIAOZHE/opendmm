1c9fb7c0998b2ef17727343e3aa1fcd96e1dadcd
Jun Zhou <pinepara@gmail.com>
2014-07-02 01:44:03 -0700

    fix kawaii

--- lib/opendmm/makers/kawaii.rb
+++ lib/opendmm/makers/kawaii.rb
@@ -10,10 +10,10 @@ private
 def self.parse_product_html(html)
   specs = Utils.hash_from_dl(html.css('#content > div.col540 > div > div.dvd_info > dl'))
   {
-    actresses:       specs['出演者'].css('a').map(&:text),
+    actresses:       specs['出演者'].text.split('/'),
     cover_image:     html.at_css('#content > div.col300 > span.pake > p.textright > a > img')['src'].gsub(/pm.jpg$/, 'pl.jpg'),
     description:     html.css('#content > div.col540 > div > p.text').text,
-    genres:          specs['ジャンル'].css('a').map(&:text),
+    genres:          specs['ジャンル'].text.split('/'),
     maker:           'Kawaii',
     movie_length:    (specs['DVD収録時間'] || specs['Blu-ray収録時間']).text,
     release_date:    (specs['DVD発売日'] || specs['Blu-ray発売日']).text,
