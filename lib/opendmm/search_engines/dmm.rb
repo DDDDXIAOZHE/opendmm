f84085478b140ad5faec2ba26017f93996991407
Jun Zhou <pinepara@gmail.com>
2014-07-10 21:53:24 -0700

    fix dmm

--- lib/opendmm/search_engines/dmm.rb
+++ lib/opendmm/search_engines/dmm.rb
@@ -1,18 +1,21 @@
 base_uri 'www.dmm.co.jp'
 
-def self.search_url(name)
+def self.search_url(name, digit_len)
   name = name.split(/(?<=[a-z])(?=\d)|[-_\s]/).map do |token|
-    token =~ /^\d{1,4}$/ ? sprintf("%05d", token.to_i) : token
-  end.join(' ')
+    token =~ /^\d+$/ ? token.rjust(digit_len, '0') : token
+  end.join
   "/search/=/searchstr=#{CGI::escape(name)}"
 end
 
 def self.product_url(name)
-  search_page = get_with_retry search_url(name)
-  return nil unless search_page
-  search_html = Utils.html_in_utf8 search_page
-  first_result = search_html.at_css('#list > li > div > p.tmb > a')
-  first_result['href'] if first_result
+  5.downto(3) do |len|
+    search_page = get_with_retry search_url(name, len)
+    next unless search_page
+    search_html = Utils.html_in_utf8 search_page
+    first_result = search_html.at_css('#list > li > div > p.tmb > a')
+    return first_result['href'] if first_result
+  end
+  nil
 end
 
 def self.product_extra_info(name, url, page, html)
@@ -26,18 +29,19 @@ end
 private
 
 def self.parse_product_html(html)
-  specs = Utils.hash_by_split(html.css('//*[@id="mu"]/div/table/tr/td[1]/table/tr').map(&:text))
+  specs = Utils.hash_by_split(html.css('//div[@class="page-detail"]/table/tr/td[1]/table/tr').map(&:text))
   {
-    actresses:       specs['出演者'].split(/[\s\/]/),
-    cover_image:     html.at_css('#sample-video > a')['href'],
-    directors:       specs['監督'].split,
-    genres:          specs['ジャンル'].split,
+    actresses:       (specs['出演者'] || specs['名前']).try(:split, /[\s\/]/),
+    cover_image:     (html.at_css('#sample-video > a').try(:[], 'href') ||
+                      html.at_css('#sample-video > img').try(:[], 'src')),
+    directors:       specs['監督'].try(:split),
+    genres:          specs['ジャンル'].try(:split),
     label:           specs['レーベル'],
     maker:           specs['メーカー'],
     movie_length:    specs['収録時間'],
-    release_date:    specs['商品発売日'],
+    release_date:    (specs['商品発売日'] || specs['配信開始日']),
     series:          specs['シリーズ'],
-    thumbnail_image: html.at_css('#sample-video > a > img')['src'],
+    thumbnail_image: html.at_css('#sample-video img').try(:[], 'src'),
     title:           html.css('#title').text,
   }
 end
