73a2b14bcb7a09355ac101cac43b38564ab2d590
Jun Zhou <pinepara@gmail.com>
2014-11-05 17:14:26 -0800

    fix madonna

--- lib/opendmm/makers/madonna.rb
+++ lib/opendmm/makers/madonna.rb
@@ -1,4 +1,4 @@
-base_uri 'madonna-av.com'
+base_uri 'www.madonna-av.com'
 
 register_product(
   /^(JUC|JUFD|JUX|OBA|URE)-?(\d{3})$/i,
@@ -8,18 +8,18 @@ register_product(
 private
 
 def self.parse_product_html(html)
-  specs = Utils.hash_from_dl(html.css('#column_contents > div > div.right_contents > dl'))
+  specs = Utils.hash_from_dl(html.css('#wrap-contents > div.wrap-detail-links > dl'))
   {
     actresses:       specs['出演女優'].css('a').map(&:text),
-    cover_image:     html.at_css('#column_contents > div > div.left_contents > div.pack > a')['href'],
-    description:     html.css('#column_contents > div > div.left_contents > p').text,
+    cover_image:     html.at_css('#wrap-contents > div.wrap-detail-main > div.pack > a')['href'],
+    description:     html.css('#wrap-contents > div.wrap-detail-main > p').text,
     directors:       specs['監督'].text.split,
     genres:          specs['ジャンル'].text.split,
     maker:           'Madonna',
     movie_length:    specs['収録時間'].text.remove('DVD'),
     release_date:    specs['発売日'].text.remove('DVD'),
-    sample_images:   html.css('#column_contents > div > div.left_contents > div.photo > ul > li > a').map { |a| a['href'] },
-    thumbnail_image: html.at_css('#column_contents > div > div.left_contents > div.pack > a > img')['src'],
-    title:           html.css('#column_contents > h2').text,
+    sample_images:   html.css('#wrap-contents > div.wrap-detail-main > div.photo > ul > li > a').map { |a| a['href'] },
+    thumbnail_image: html.at_css('#wrap-contents > div.wrap-detail-main > div.pack > a > img')['src'],
+    title:           html.css('#wrap-contents > h1').text,
   }
 end
