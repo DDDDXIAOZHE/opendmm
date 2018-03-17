b4d9f42642dd09a7c1cccf913cc4dd8432ef5e95
LibreDMM <admin@libredmm.com>
2018-03-16 23:05:30 -0700

    [dmm] fix release date cont.

--- dmm.go
+++ dmm.go
@@ -110,7 +110,7 @@ func dmmParse(urlstr string, keyword string, metach chan MovieMeta) {
 			td := tr.Find("td").First()
 			k := td.Text()
 			v := td.Next()
-			if strings.Contains(k, "開始日") {
+			if strings.Contains(k, "開始日") || strings.Contains(k, "発売日") {
 				meta.ReleaseDate = v.Text()
 			} else if strings.Contains(k, "収録時間") {
 				meta.MovieLength = v.Text()
